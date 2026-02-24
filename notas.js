45085f211036964c4e73b89d4390a4317cb4510a
larissacamargo-hash <larissa.barbosa.santos19@escola.pr.gov.br>
2026-02-24 07:55:52 -0300

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,7 @@
+aluno="Pedro";
+nota1=50;
+nota2=70;
+nota3=90;
+nota4=80;
+media=(nota1 + nota2 + nota3 + nota4) /4;
+console.log("O aluno", aluno, "tem a media", media);
---
+++ teste3.js
@@ -0,0 +1,7 @@
+Delta=0;
+b=5;
+a=2;
+c=8;
+
+Delta = b*b*4*a*c;
+console.log("O...é, Delta");
