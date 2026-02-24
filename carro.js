19517de084917b60aead22d58e6925e6a1eeabe2
larissacamargo-hash <larissa.barbosa.santos19@escola.pr.gov.br>
2026-02-24 08:59:36 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,13 @@
+carro1 = "Impala"
+carro2 = "Voyage"
+
+kmcarro1=100;
+kmcarro2=120;
+
+precokm1= 20;
+precokm2= 35;
+
+totalcarro1=(kmcarro1*precokm1);
+totalcarro2=(kmcarro2*precokm2);
+
+console.log(totalcarro1, totalcarro2);
